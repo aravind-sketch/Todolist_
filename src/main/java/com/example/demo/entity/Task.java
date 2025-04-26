49de600011d070190b0a77442a00124a52cc78f5
aravind-sketch <aravindongole2@gmail.com>
2025-04-27 01:01:03 +0530

    initial commit

---
+++ src/main/java/com/example/demo/entity/Task.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private boolean completed;
+}
